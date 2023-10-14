15fbb910c0ba94eb2c4f788a935b1d9593fd90a0
Chey Salem <csalem@nmsu.edu>
2023-10-14 16:41:22 -0600

    test

--- py/app.py
+++ py/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', 'about.html')
 
 @app.route('/submit_data', methods=['POST'])
 def submit_data():
