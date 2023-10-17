dd4551c4c287fdea7a4cc05a854b09fd4600adf9
davidkuntz02 <davidkuntz02@gmail.com>
2023-10-16 20:39:16 -0600

    analysis js called on every page now

--- js/analysis.js
+++ js/analysis.js
@@ -2,18 +2,6 @@
 //NMSU CS485 Fall 2023 Project Pebble Village
 //I did most of this - David K
 
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const fs = require('fs');
-
-// app.use(cors());
-// console.out("cors");
-//import { fs } from 'node:fs';
-
-//generate unique id on execution
-const id = Date.now().toString(36) + Math.random().toString(36).substring(2);
-
 //example script from w3
 //generates a cookie with a name, text value, and expiration value
   function setCookie(cname, cvalue, exdays) {
@@ -43,41 +31,36 @@ const id = Date.now().toString(36) + Math.random().toString(36).substring(2);
     return "";
   }
 
-  //this is a contingency effort for if we can't get a database working
-  // function writeToFile(data){
-  //   fs.appendFileSync('fingerprints.json', data, (err) => {
-  //     // In case of a error throw err.
-  //     if (err) throw err;
-  // })
-  // }
-
   //collect as much information about the user in the background as possible
   //store it in a key value pair object
   //and sends it to the database
   function fingerprint(){
+    //create data object
     const fp = {
-      dID : id, //device visit id
-      //navigator properties
-      cookies : navigator.cookieEnabled,//cookieenabled
-      langu : navigator.languages,//languages
+      //generate unique device visit id 
+      dID : Date.now().toString(36) + Math.random().toString(36).substring(2), 
       useragent : navigator.userAgent,//useragent
-      platform : navigator.platform,//browser platform
       engine : navigator.engine,//browser engine
+      aheight : screen.availHeight,//available height
+      awidth : screen.availWidth,//available width
+
+      //slimmed down the actual data collected
+
+      //navigator properties
+      //cookies : navigator.cookieEnabled//cookieenabled
+      //langu : navigator.languages,//languages
+      //platform : navigator.platform,//browser platform
       //TODO add these categories to database
-      java : navigator.java,//java enabled?
-      dnt : navigator.doNotTrack,//donottrack enabled
-      cores : navigator.hardwareConcurrency,//number of cores or idk
+      //java : navigator.java,//java enabled?
+      //dnt : navigator.doNotTrack,//donottrack enabled
+      //cores : navigator.hardwareConcurrency,//number of cores or idk
       //screen properties
-      h : screen.height,//height
-      ah : screen.availHeight,//available height
-      w : screen.width,//width
-      sw : screen.availWidth,//available width
-      getID : function(){
-        return id;
-      }
+      //h : screen.height,//height
+      //w : screen.width,//width
     };
 
-    //send visitor data to database TODO
+    //send visitor data to the worker
+    //if we actually got this working I think it would involve a fetch API here
     let fpOUT = JSON.stringify(fp);
     console.log(fpOUT);
 
@@ -102,6 +85,7 @@ const id = Date.now().toString(36) + Math.random().toString(36).substring(2);
   } 
 
   //secret cookie button function
+  //use this to check if a cookie was set or not
   function showCookies(){
     if(document.cookie != ""){
       fpee = JSON.stringify(document.cookie);
